487a9b0827c861c9a26fd2f87e2c96ac075efa16
ArvindKumar <arvind.kumar@dal.ca>
2019-09-22 22:54:02 +0000

    first commit

---
+++ task1.py
@@ -0,0 +1,18 @@
+def nested_sum(nestedList):
+        newList = []
+        #Helper function to flatten the list
+        def flatlist(nestedList):
+                '''
+                Returns a flat list
+                '''
+                for i in range(len(nestedList)):
+                        if type(nestedList[i]) == int:
+                                newList.append(nestedList[i])
+                        else:
+                                flatlist(nestedList[i])
+                return newList
+
+        flatlist(nestedList)
+        print(sum(newList))
+        
+nested_sum(nestedList)
